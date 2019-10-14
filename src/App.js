1da91bc7875bc832468a6d1d319e6ab5542e32bf
Brian <44780125+BrianEpaud@users.noreply.github.com>
2019-10-14 16:45:02 +0200

    todo css

--- src/App.js
+++ src/App.js
@@ -1,26 +1,56 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      items: [
+        {text:"learn React", key:Date.now()}
+      ],
+      input:''
+    }
+  }
+  handleChange = (e) => {
+    this.setState({input:e.target.value})
+  }
+
+  add = () => {
+    // this.setState({
+    //   items:this.state.items.concat( {text:this.state.input, key:Date.now()})
+    // })
+
+    let newItem = {text:this.state.input, key:Date.now()}
+
+    this.setState(state =>( {
+      items:[newItem].concat(state.items),
+      input : ''
+    }))
+    
+  }
+  render() {
+    return (
+      <div id="container">
+        <div className="todoListMain">
+          <div className="header">
+            <form onSubmit={(e) => {e.preventDefault(); this.add()}}>
+              <input 
+              onChange={(e) => this.handleChange(e)} 
+              className="rounded" 
+              placeholder="Ajouter une tâche"
+              value={this.state.input}
+              />
+              <button className="rounded">Ajouter</button>
+            </form>
+            <ul className="theList">
+              {this.state.items.map(item => {
+                return(<li key={item.key}>{item.text} </li>)
+              })}
+            </ul>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
-export default App;
